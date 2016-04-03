21448b5afc5d323e98936e06015fa58867b1464b
Icy1-0-6 <ivo1119@live.nl>
2016-04-03 23:00:12 +0200

    Added alot of functions, added background worker, status label, quit button.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace KarmaConvertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Forge = new System.Windows.Forms.Timer(this.components);
+            this.Realforge = new System.Windows.Forms.Timer(this.components);
+            this.testTimer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblXas = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblYas = new System.Windows.Forms.Label();
+            this.MouseTrackTimer = new System.Windows.Forms.Timer(this.components);
+            this.Status = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // Forge
+            // 
+            this.Forge.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Realforge
+            // 
+            this.Realforge.Tick += new System.EventHandler(this.Realforge_Tick);
+            // 
+            // testTimer1
+            // 
+            this.testTimer1.Tick += new System.EventHandler(this.testTimer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lblXas
+            // 
+            this.lblXas.AutoSize = true;
+            this.lblXas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblXas.Location = new System.Drawing.Point(25, 102);
+            this.lblXas.Name = "lblXas";
+            this.lblXas.Size = new System.Drawing.Size(52, 20);
+            this.lblXas.TabIndex = 2;
+            this.lblXas.Text = "lblXas";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Set PackCount";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Return PackCount value";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblYas
+            // 
+            this.lblYas.AutoSize = true;
+            this.lblYas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYas.Location = new System.Drawing.Point(25, 122);
+            this.lblYas.Name = "lblYas";
+            this.lblYas.Size = new System.Drawing.Size(52, 20);
+            this.lblYas.TabIndex = 5;
+            this.lblYas.Text = "lblYas";
+            // 
+            // MouseTrackTimer
+            // 
+           
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(29, 181);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(128, 13);
+            this.Status.TabIndex = 6;
+            this.Status.Text = "Currently: Waiting for start";
+            this.Status.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.lblYas);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblXas);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Forge;
+        private System.Windows.Forms.Timer Realforge;
+        private System.Windows.Forms.Timer testTimer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblXas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblYas;
+        private System.Windows.Forms.Timer MouseTrackTimer;
+        private System.Windows.Forms.Label Status;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace KarmaConvertor
+{
+    
+    /* To do list:
+     * - Make it open and close the inventory.(DONE)
+     * - Close inventory before going to buy.(In place but needs implementing
+     * - Open inventory after buying.(In place but needs implementing
+     * - Auto open the mystic forge.
+     * - Auto forge the right amount of times.
+     * - Open inventory after done with forge.(In place but needs implementing
+     * - Make it automatically set the forge, merchant and bag in right posisions.
+     */
+
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker bw = new BackgroundWorker();
+
+        //________________________VAR LIST:________________________________
+        double statusindicator = 0;
+        double buysession;
+        double forgesession;
+        //_________________________________________________________________
+
+
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        private const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        
+
+        public bool isPressed(Keys vKey)
+        {
+            return Convert.ToBoolean(GetAsyncKeyState(vKey));
+        }
+
+        
+        // the magic that makes it all happen
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Random r = new Random();
+            
+
+            if (isPressed(Keys.F5))
+            {
+                System.Threading.Thread.Sleep(r.Next(150, 250));
+                //Set status:
+                statusindicator = 0;
+                //Focus on Merchant window
+                Cursor.Position = new Point((r.Next(532, 845)), (r.Next(23, 24)));
+                System.Threading.Thread.Sleep(r.Next(25, 50));
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(50, 100));
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(200, 350));
+
+                // Loops throug the buy process till buysession =  var i
+                for (int b = 0; b < buysession; b++)  
+                {
+
+                    //Set status:
+                    statusindicator = 1;
+                    
+                    //Click buy
+                    Cursor.Position = new Point((r.Next(924, 967)), (r.Next(640, 648)));
+                    System.Threading.Thread.Sleep(r.Next(50, 75));
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(r.Next(300, 400));
+
+                    // Click yes
+                    Cursor.Position = new Point((r.Next(922, 1017)), (r.Next(587, 595)));
+                    System.Threading.Thread.Sleep(r.Next(50, 75));
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(r.Next(300, 350));
+                }
+
+
+                //Set status:
+                statusindicator = 2;
+
+                //Focus Inventory window
+                Cursor.Position = new Point((r.Next(1525, 1777)), (r.Next(15, 28)));
+                System.Threading.Thread.Sleep(r.Next(25, 50));
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(50, 100));
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(50, 100));
+
+                //Set status:
+                statusindicator = 3;
+
+                //Open Forge(click 1st bag slot)
+                Cursor.Position = new Point((r.Next(1525, 1541)), (r.Next(124, 147)));
+                System.Threading.Thread.Sleep(r.Next(50, 75));
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(25, 50));
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(25));
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(25, 50));
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(250, 350));
+
+                //Forge looping
+                for (int a = 0; a < forgesession; a++)
+                {
+
+                    //Set status:
+                    statusindicator = 4;
+
+                    //obj 1
+                    Cursor.Position = new Point((r.Next(520, 560)), (r.Next(240, 290)));
+                    System.Threading.Thread.Sleep(r.Next(50, 201));
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(r.Next(50, 78));
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(r.Next(50, 201));
+                    // Obj 2
+                    Cursor.Position = new Point((r.Next(577, 627)), (r.Next(240, 290)));
+                    System.Threading.Thread.Sleep(r.Next(124, 201));
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(50);
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(r.Next(50, 201));
+                    //Obj 3
+                    Cursor.Position = new Point((r.Next(640, 685)), (r.Next(240, 290)));
+                    System.Threading.Thread.Sleep(r.Next(50, 201));
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(50);
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(r.Next(50, 201));
+                    //obj 4
+                    Cursor.Position = new Point((r.Next(695, 745)), (r.Next(240, 290)));
+                    System.Threading.Thread.Sleep(r.Next(50, 201));
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(50);
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(r.Next(102, 231));
+                    //press Ok.
+                    Cursor.Position = new Point((r.Next(1080, 1125)), (r.Next(700, 715)));
+                    System.Threading.Thread.Sleep(125);
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    System.Threading.Thread.Sleep(r.Next(1703, 1852));
+                }
+
+                //Open Salvage kit(click 2nd bag slot)
+                Cursor.Position = new Point((r.Next(1573, 1603)), (r.Next(125, 149)));
+                System.Threading.Thread.Sleep(r.Next(50, 75));
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(25, 50));
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(25));
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(25, 50));
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                System.Threading.Thread.Sleep(r.Next(250, 350));
+                //Set status:
+                statusindicator = 5;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Status.ForeColor = System.Drawing.Color.Green;
+            Status.Text = "Currently: Pending";
+            Forge.Enabled = true;
+            Realforge.Enabled = true;
+            testTimer1.Enabled = true;
+            MouseTrackTimer.Enabled = true;
+        }
+
+
+       // TEST AREA //
+        public void button1_Click(object sender, EventArgs e)
+        {
+            double Value1 = float.Parse(textBox1.Text);
+            double divideby = 4;
+            double Calcstuff = Value1 / divideby;
+            double Rounded1 = Math.Floor(Calcstuff);
+            this.buysession = Rounded1 * 4;
+            this.forgesession = buysession / 4;
+            { 
+                lblXas.Text = "you can get " + Rounded1.ToString() +" sets";
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(forgesession.ToString());
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync(); 
+            }
+
+            if (isPressed(Keys.F6))
+            {
+                this.Close();
+            }
+
+            lblXas.Text = " X =" + " " + Cursor.Position.X;
+            lblYas.Text = " Y =" + " " + Cursor.Position.Y;
+
+            //Update otherlable 
+            if (statusindicator == 0)
+            {
+                Status.ForeColor = System.Drawing.Color.Green;
+                Status.Text = "Currently: Pending";
+            }
+
+            if (statusindicator == 1)
+            {
+                Status.ForeColor = System.Drawing.Color.Green;
+                Status.Text = "Currently: Buying gloves"; ;
+            }
+
+            if (statusindicator == 2)
+            {
+                Status.ForeColor = System.Drawing.Color.Green;
+                Status.Text = "Currently: Selecting inventory";
+            }
+
+            if (statusindicator == 3)
+            {
+                Status.ForeColor = System.Drawing.Color.Green;
+                Status.Text = "Currently: Opening Forge";
+            }
+
+            if (statusindicator == 4)
+            {
+                Status.ForeColor = System.Drawing.Color.Green;
+                Status.Text = "Currently: Forgeing gloves";
+            }
+            if (statusindicator == 5)
+            {
+                Status.ForeColor = System.Drawing.Color.Green;
+                Status.Text = "Currently: Pending/Wait for salvage";
+            }
+
+        }
+
+
+
+        // TEST MOVING THE WINDOW //
+        private void testTimer1_Tick(object sender, EventArgs e)
+        {
+            if (isPressed(Keys.F7))
+            {
+                Random r = new Random();
+                //Move the window to posision X/Y (BAG)  (WORKS)
+                //mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                //Cursor.Position = new Point(1524, 6);
+                //System.Threading.Thread.Sleep(125);
+                //mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+                //Move the window to posision X/Y (Vendor)  (WORKS)
+                //mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                //Cursor.Position = new Point(553, 12);
+                //System.Threading.Thread.Sleep(125);
+                //mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+                //Open inventory(WORKS)
+                //Cursor.Position = new Point((r.Next(105, 118)), (r.Next(10, 24)));
+                //mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(50, 100));
+                //mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(50, 100));
+
+                //Focus on Merchant window
+                //Cursor.Position = new Point((r.Next(529, 845)), (r.Next(22, 24)));
+                //System.Threading.Thread.Sleep(r.Next(25, 50));
+                //mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(50, 100));
+                //mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(50, 100));
+
+                //Focus Inventory window
+                //Cursor.Position = new Point((r.Next(1525, 1777)), (r.Next(15, 28)));
+                //System.Threading.Thread.Sleep(r.Next(25, 50));
+                //mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(50, 100));
+                //mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(50, 100));
+
+                //Open Forge(click 1st bag slot)
+                //Cursor.Position = new Point((r.Next(1525, 1541)), (r.Next(124, 147)));
+                //System.Threading.Thread.Sleep(r.Next(50, 75));
+                //mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(25, 50));
+                //mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(25));
+                //mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(25, 50));
+                //mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                //System.Threading.Thread.Sleep(r.Next(250, 350));
+                //this.Close();
+
+            }
+        }
+
+        private void Realforge_Tick(object sender, EventArgs e)
+        {
+            //if (isPressed(Keys.F6))
+            //{
+            //    Random r = new Random();
+            //    //obj 1
+            //    Cursor.Position = new Point((r.Next(520, 560)), (r.Next(240, 290)));
+            //    System.Threading.Thread.Sleep(r.Next(50, 201));
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            //    System.Threading.Thread.Sleep(r.Next(50, 78));
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            //    System.Threading.Thread.Sleep(r.Next(50, 201));
+            //    // Obj 2
+            //    Cursor.Position = new Point((r.Next(577, 627)), (r.Next(240, 290)));
+            //    System.Threading.Thread.Sleep(r.Next(124, 201));
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            //    System.Threading.Thread.Sleep(50);
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            //    System.Threading.Thread.Sleep(r.Next(50, 201));
+            //    //Obj 3
+            //    Cursor.Position = new Point((r.Next(640, 685)), (r.Next(240, 290)));
+            //    System.Threading.Thread.Sleep(r.Next(50, 201));
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            //    System.Threading.Thread.Sleep(50);
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            //    System.Threading.Thread.Sleep(r.Next(50, 201));
+            //    //obj 4
+            //    Cursor.Position = new Point((r.Next(695, 745)), (r.Next(240, 290)));
+            //    System.Threading.Thread.Sleep(r.Next(50, 201));
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            //    System.Threading.Thread.Sleep(50);
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            //    System.Threading.Thread.Sleep(r.Next(102, 231));
+            //    //press Ok.
+            //    Cursor.Position = new Point((r.Next(1080, 1125)), (r.Next(700, 715)));
+            //    System.Threading.Thread.Sleep(125);
+            //    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            //    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            //    System.Threading.Thread.Sleep(r.Next(1703, 1852));
+            //}
+        }
+
+
+
+
+
+
+
+
+    }
+}
